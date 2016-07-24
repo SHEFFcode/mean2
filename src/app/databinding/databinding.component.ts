2824c3031a039fd3a4c3ade50d8b7c53c6dd2762
SHEFFcode <sheff@sheffmachine.com>
2016-07-23 17:20:49 -0700

    bind to default events

--- src/app/databinding/databinding.component.ts
+++ src/app/databinding/databinding.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {PropertyBdingComponent} from "./property-bding.component";
+import {EventBindingComponent} from "./event-binding.component";
 
 @Component({
   moduleId: module.id,
   selector: 'app-databinding',
   templateUrl: 'databinding.component.html',
   styleUrls: ['databinding.component.css'],
-  directives: [PropertyBdingComponent]
+  directives: [PropertyBdingComponent, EventBindingComponent]
 })
 export class DatabindingComponent {
   stringInterpolation = 'This is string interpolation';
