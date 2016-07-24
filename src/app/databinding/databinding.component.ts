d9467837bb67ebe28279b6f9ae0872c2db866260
SHEFFcode <sheff@sheffmachine.com>
2016-07-23 17:36:31 -0700

    two way databinding

--- src/app/databinding/databinding.component.ts
+++ src/app/databinding/databinding.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import {PropertyBdingComponent} from "./property-bding.component";
 import {EventBindingComponent} from "./event-binding.component";
+import {TwoWayBindingComponent} from "./two-way-binding.component";
 
 @Component({
   moduleId: module.id,
   selector: 'app-databinding',
   templateUrl: 'databinding.component.html',
   styleUrls: ['databinding.component.css'],
-  directives: [PropertyBdingComponent, EventBindingComponent]
+  directives: [PropertyBdingComponent, EventBindingComponent, TwoWayBindingComponent]
 })
 export class DatabindingComponent {
   stringInterpolation = 'This is string interpolation';
